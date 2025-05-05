b3e42be1b99d5f58c8924ad245dd18be85f7011a
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 11:50:30 +0200

    test(diamond): 🔴 middle as `B B`

--- kata/diamond/src/diamond.test.ts
+++ kata/diamond/src/diamond.test.ts
@@ -8,8 +8,14 @@ import { prettyDiamond } from "./diamond";
 // - [ ] When C is given, return two ` B B `'s
 // - [ ] When C is given, return head and tail as `  A  `
 
-describe("only A", () => {
+describe("A", () => {
   it("return only A", () => {
     expect(prettyDiamond("A")).toEqual(`A`);
+  });
+});
+
+describe("B", () => {
+  it("middle as `B B`", () => {
+    expect(prettyDiamond("B")).toInclude(`B B`);
   });
 });
