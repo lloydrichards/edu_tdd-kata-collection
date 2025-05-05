751d54246243017b72bda55ac1814d96044d2ab7
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 11:59:47 +0200

    test(diamond): 🟢 middle as `C   C`

--- kata/diamond/src/diamond.ts
+++ kata/diamond/src/diamond.ts
@@ -1,4 +1,6 @@
 export const prettyDiamond = (letter: string) => {
   if (letter === "A") return "A";
-  return " A \nB B\n A ";
+  if (letter === "B") return " A \nB B\n A ";
+
+  return "C   C"
 };
