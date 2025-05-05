8886753df7e6690b9e69136803f0d6cbc28fba61
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 13:57:27 +0200

    test(diamond): 🔴 B should be a diamond

--- kata/diamond/src/diamond.ts
+++ kata/diamond/src/diamond.ts
@@ -1,17 +1,6 @@
 export const prettyDiamond = (letter: string) => {
   const alphabet = ["A", "B", "C", "D", "E"];
 
-  if (letter === "B") {
-    const alpha = alphabet.findIndex((d) => d == "B");
-    const facets = [
-      alphabet[alpha - 1]!.padEnd(alpha + 1, " ").padStart(alpha + 2, " "),
-    ];
-    return [
-      ...facets,
-      alphabet[alpha]!.padEnd(alpha + 1, " ")! + alphabet[alpha]!,
-      ...facets.toReversed(),
-    ];
-  }
   if (letter === "C") {
     const alpha = alphabet.findIndex((d) => d == "C");
     const point = alphabet[0]!.padEnd(alpha + 1, " ").padStart(alpha + 3, " ");
