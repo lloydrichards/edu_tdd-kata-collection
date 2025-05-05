9726f2d10defb2fdfba09b5dc6225a9ecd4939e7
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 12:01:10 +0200

    test(diamond): 🟢 return start and end with ` A `'s

--- kata/diamond/src/diamond.ts
+++ kata/diamond/src/diamond.ts
@@ -2,5 +2,5 @@ export const prettyDiamond = (letter: string) => {
   if (letter === "A") return "A";
   if (letter === "B") return " A \nB B\n A ";
 
-  return "C   C"
+  return "  A  C   C  A  ";
 };
