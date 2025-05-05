35f8ac228e66403a51c6ec83f3ce6efb99ff4f6e
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 10:36:57 +0200

    refactor: 🟡 pass

--- files/yatze.ts
+++ files/yatze.ts
@@ -1,11 +1,14 @@
 
+
+const sum = (numbers: Array<number>) =>numbers.reduce((acc,cur)=>acc+cur,0); 
+
 export const roll = (dices: Array<number>, type: string) => {
   if(type==="Yatze"){
    return dices.reduce((acc,cur)=>acc && cur===dices[0],true) ? 50 : 0; 
   }
   if(type==="Ones"){
     const filtered = dices.filter(d=>d===1)
-   return filtered.length === 2 ? filtered.reduce((acc,cur)=>acc+cur,0) : 0
+   return filtered.length === 2 ? sum(filtered) : 0
   }
- return dices.reduce((acc,cur)=>acc+cur,0); 
+ return sum(dices); 
 }
