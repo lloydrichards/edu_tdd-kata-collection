657255ce3cf45ac89bd7afdc61454e41a12ace64
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 11:48:11 +0200

    refactor(diamond): 🟡 remove answer

--- kata/diamond/src/diamond.test.ts
+++ kata/diamond/src/diamond.test.ts
@@ -1,13 +1,7 @@
-import { answer, prettyDiamond } from "./hiker";
-import { expect, describe, it } from "bun:test";
+import { describe, expect, it } from "bun:test";
+import { prettyDiamond } from "./diamond";
 
-describe("answer", () => {
-  it("to life the universe and everything", () => {
-    expect(answer()).toEqual(42);
-  });
-});
-
-// - [ ] When A is given, return only A
+// - [x] When A is given, return only A
 // - [ ] When B is given, return middle as `B B`
 // - [ ] When B is given, return head and tail as ` A `'s
 // - [ ] When C is given, return middle as `C  C`
--- kata/diamond/src/diamond.ts
+++ kata/diamond/src/diamond.ts
@@ -1,5 +1,3 @@
-export const answer = () => 6 * 7;
-
 export const prettyDiamond = (letter: string) => {
   return letter;
 };
