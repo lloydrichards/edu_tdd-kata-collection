686589f29205e761ce2615d9cf5b6a88a059a5cf
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 10:36:57 +0200

    test: 🟢 pass

--- files/yatze.ts
+++ files/yatze.ts
@@ -5,7 +5,7 @@ export const roll = (dices: Array<number>, type: string) => {
   }
   if(type==="Ones"){
     const filtered = dices.filter(d=>d===1)
-   return filtered.length === 2 ? dices.reduce((acc,cur)=>acc+cur,0) : 0
+   return filtered.length === 2 ? filtered.reduce((acc,cur)=>acc+cur,0) : 0
   }
  return dices.reduce((acc,cur)=>acc+cur,0); 
 }
