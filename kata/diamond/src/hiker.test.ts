5034d3d70936d5be9829f83c43554d187aada51c
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 11:44:19 +0200

    docs: plan diamond tasks

--- kata/diamond/src/hiker.test.ts
+++ kata/diamond/src/hiker.test.ts
@@ -6,3 +6,10 @@ describe("answer", () => {
     expect(answer()).toEqual(42);
   });
 });
+
+// - [ ] When A is given, return only A
+// - [ ] When B is given, return middle as `B B`
+// - [ ] When B is given, return head and tail as ` A `'s
+// - [ ] When C is given, return middle as `C  C`
+// - [ ] When C is given, return two ` B B `'s
+// - [ ] When C is given, return head and tail as `  A  `
