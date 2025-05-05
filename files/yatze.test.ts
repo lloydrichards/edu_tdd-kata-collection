a2a014ba155451426e6160b40f069b3f3923e92d
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 10:36:57 +0200

    refactor: 🟡 pass

--- files/yatze.test.ts
+++ files/yatze.test.ts
@@ -10,6 +10,7 @@ describe('Chance', () => {
 describe('Yatze', () => {
   it('roll 5 dice successfully', () => {
     expect(roll([1,1,1,1,1],"Yatze")).toEqual(50);
+    expect(roll([5,5,5,5,5],"Yatze")).toEqual(50);
   });
     it('roll 5 dice unsuccessfully', () => {
     expect(roll([1,1,1,2,1],"Yatze")).toEqual(0);
