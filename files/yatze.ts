fc7c28d7df26b9d9e6dd7b3f8d9cbecb5e3e8983
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 10:36:57 +0200

    test: 🔴 failed

--- files/yatze.ts
+++ files/yatze.ts
@@ -3,5 +3,9 @@ export const roll = (dices: Array<number>, type: string) => {
   if(type==="Yatze"){
    return dices.reduce((acc,cur)=>acc && cur===dices[0],true) ? 50 : 0; 
   }
+  if(type==="Ones"){
+    const filtered = dices.filter(d=>d===1)
+   return filtered.length === 2 ? dices.reduce((acc,cur)=>acc+cur,0) : 0
+  }
  return dices.reduce((acc,cur)=>acc+cur,0); 
 }
