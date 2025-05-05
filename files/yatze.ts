e91a9ae1e6ad840dec542c010f69711e0028b744
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 10:36:58 +0200

    test: 🟢 pass

--- files/yatze.ts
+++ files/yatze.ts
@@ -14,5 +14,21 @@ export const roll = (dices: Array<number>, type: string) => {
     const filtered = dices.filter(d=>d===2)
    return filtered.length >= 2 ? sum(filtered.slice(0,2)) : 0
   }
+    if(type==="Threes"){
+    const filtered = dices.filter(d=>d===3)
+   return filtered.length >= 2 ? sum(filtered.slice(0,2)) : 0
+  }
+    if(type==="Fours"){
+    const filtered = dices.filter(d=>d===4)
+   return filtered.length >= 2 ? sum(filtered.slice(0,2)) : 0
+  }
+    if(type==="Fives"){
+    const filtered = dices.filter(d=>d===5)
+   return filtered.length >= 2 ? sum(filtered.slice(0,2)) : 0
+  }
+    if(type==="Sixes"){
+    const filtered = dices.filter(d=>d===6)
+   return filtered.length >= 2 ? sum(filtered.slice(0,2)) : 0
+  }
  return sum(dices); 
 }
