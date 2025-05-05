9a3fea2171d6d508e1d7c66e1b2bcb42cfa2f5af
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 10:36:58 +0200

    test: 🟢 pass

--- files/yatze.ts
+++ files/yatze.ts
@@ -27,7 +27,15 @@ export const roll = (dices: Array<number>, type: string) => {
     case "Sixes":
       return doubleScorer(6);
     case "Pairs":
-      return 8;
+      const hasPairOfOnes = dices.filter(d=>d===1).length >= 2
+      const hasPairOfTwos = dices.filter(d=>d===2).length >= 2
+      const hasPairOfThrees = dices.filter(d=>d===3).length >= 2
+      const hasPairOfFours = dices.filter(d=>d===4).length >= 2
+      const hasPairOfFives = dices.filter(d=>d===5).length >= 2
+      const hasPairOfSixes = dices.filter(d=>d===6).length >= 2
+      
+      const combo = [hasPairOfOnes,hasPairOfTwos,hasPairOfThrees,hasPairOfFours,hasPairOfFives,hasPairOfSixes].filter(Boolean)
+      return combo.length ==2 ? 8 : 0;
     default:
       return sum(dices);
   }
