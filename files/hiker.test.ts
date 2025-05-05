9659d9d89f6fc611c40009adf25ea6e58657c39d
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-05 10:36:57 +0200

    test: 🔴 failed

--- files/hiker.test.ts
+++ files/hiker.test.ts
@@ -3,5 +3,6 @@ import {  roll } from './hiker';
 describe('Chance', () => {
   it('roll 5 dice', () => {
     expect(roll([1,1,3,3,6],"Chance")).toEqual(14);
+    expect(roll([4,5,5,6,1],"Chance")).toEqual(21);
   });
 });
