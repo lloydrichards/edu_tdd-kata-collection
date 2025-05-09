55e876ab11f2a629764ee42d33e58549419a5333
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-09 08:24:11 +0200

    test(poker): 🔴 should accept two hand of cards

--- kata/poker_hands/src/hiker.test.ts
+++ kata/poker_hands/src/hiker.test.ts
@@ -1,4 +1,4 @@
-import { answer } from "./hiker";
+import { answer, pokerGame } from "./hiker";
 import { expect, describe, it } from "bun:test";
 
 // - [ ] should accept two hand of cards
@@ -18,5 +18,11 @@ import { expect, describe, it } from "bun:test";
 describe("answer", () => {
   it("to life the universe and everything", () => {
     expect(answer()).toEqual(42);
+  });
+});
+
+describe("pokerGame", () => {
+  it("should accept two hand of cards", () => {
+    expect(pokerGame(["", "", "", "", ""], ["", "", "", "", ""])).toBe("");
   });
 });
--- kata/poker_hands/src/hiker.ts
+++ kata/poker_hands/src/hiker.ts
@@ -1,1 +1,6 @@
 export const answer = () => 6 * 9;
+
+export const pokerGame = (black: Array<string>, white: Array<string>) => {
+  throw new Error("not implemented");
+  return "";
+};
