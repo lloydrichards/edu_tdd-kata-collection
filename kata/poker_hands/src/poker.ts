b108fce24547828aecf19ee4558f2bf8870969fe
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-09 09:01:06 +0200

    🟢 should accept T (10) card

--- kata/poker_hands/src/poker.ts
+++ kata/poker_hands/src/poker.ts
@@ -14,12 +14,13 @@ const getSuit = (card: string) => {
 };
 
 export const card = (card: string) => {
-  const validValues: Array<any> = ["2", "3", "4", "5", "6", "7", "8", "9"];
+  const validValues: Array<any> = ["2", "3", "4", "5", "6", "7", "8", "9", "T"];
   const value = card[0];
-  if (!validValues.includes(value)) throw new Error("value is not accepted");
+  const valueIdx = validValues.findIndex((v) => v === value);
+  if (valueIdx === -1) throw new Error("value is not accepted");
   return {
     suit: getSuit(card),
-    value: Number(value),
+    value: valueIdx + 2,
   };
 };
 
