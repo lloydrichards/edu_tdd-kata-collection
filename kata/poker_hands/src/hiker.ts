2fc81e18431131da4ed92c63c02f75e04226a9bb
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-09 08:26:22 +0200

    test(poker): 🟢 should accept two hand of cards

--- kata/poker_hands/src/hiker.ts
+++ kata/poker_hands/src/hiker.ts
@@ -1,6 +1,5 @@
-export const answer = () => 6 * 9;
+export const answer = () => 6 * 7;
 
 export const pokerGame = (black: Array<string>, white: Array<string>) => {
-  throw new Error("not implemented");
   return "";
 };
