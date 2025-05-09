28e298b567f8796019526addff7a86b108275d56
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-09 09:02:27 +0200

    🟡 extract getValue

--- kata/poker_hands/src/poker.ts
+++ kata/poker_hands/src/poker.ts
@@ -13,14 +13,20 @@ const getSuit = (card: string) => {
   }
 };
 
-export const card = (card: string) => {
+const getValue = (card: string) => {
   const validValues: Array<any> = ["2", "3", "4", "5", "6", "7", "8", "9", "T"];
   const value = card[0];
   const valueIdx = validValues.findIndex((v) => v === value);
+
   if (valueIdx === -1) throw new Error("value is not accepted");
+
+  return valueIdx + 2;
+};
+
+export const card = (card: string) => {
   return {
     suit: getSuit(card),
-    value: valueIdx + 2,
+    value: getValue(card),
   };
 };
 
