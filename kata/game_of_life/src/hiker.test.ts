b39ddfee53053355952332809d5c8c44190df1b9
Lloyd Richards <lloyd.d.richards@gmail.com>
2025-05-17 19:54:40 +0200

    🔴 when given an input, should return a generation

--- kata/game_of_life/src/hiker.test.ts
+++ kata/game_of_life/src/hiker.test.ts
@@ -1,8 +1,18 @@
-import { answer } from "./hiker";
-import { expect, describe, it } from "vitest";
+import { Effect } from "effect";
+import { answer, GameOfLife } from "./hiker";
+import { expect, describe, it } from "@effect/vitest";
 
 describe("answer", () => {
   it("to life the universe and everything", () => {
     expect(answer()).toEqual(42);
   });
+});
+
+describe("gameOfLife", () => {
+  it.effect("when given an input, should return a generation", () =>
+    Effect.gen(function* () {
+      const result = yield* GameOfLife("");
+      expect(result).toBe("");
+    })
+  );
 });
--- kata/game_of_life/src/hiker.ts
+++ kata/game_of_life/src/hiker.ts
@@ -1,1 +1,7 @@
+import { Effect } from "effect";
+
 export const answer = () => 6 * 9;
+
+export const GameOfLife = Effect.fn("GameOfLife")(function* (n: string) {
+  return null;
+});
